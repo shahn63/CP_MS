2bdb1ad8b63366bc4b3e71ec3e4f3a1c30124071
shahn63 <132188355+shahn63@users.noreply.github.com>
2024-10-01 01:45:14 +0900

    Update CPMS_band.R

--- CPMS_band.R
+++ CPMS_band.R
@@ -1,5 +1,5 @@
 
-CPMS_band<-function(CPMS.obj=CPMS_result, test=test_dat, id=detected_sample_ID, peak.plot=T){
+CPMS_band<-function(CPMS.obj=CPMS_result, test=test_dat, id=detected_sample_ID, alpha=0.05, peak.plot=T){
 	G<-CPMS.obj$numG
 	ff.idx<-1:G
 	Lt<-CPMS.obj$mag.lower
@@ -20,17 +20,18 @@ CPMS_band<-function(CPMS.obj=CPMS_result, test=test_dat, id=detected_sample_ID, 
 
 	if(peak.plot==T){
 	linet<-c(1:length(ff.idx)+1,1)
-	layout(matrix(1:(length(ff.idx)+1), ncol = 1, byrow = TRUE), heights = c(2,2,2,1.7))
+	layout(matrix(1:(length(ff.idx)+1), ncol = 1, byrow = TRUE), heights = c(rep(2,length(ff.idx)),1.2))
+	par(mar = c(1.9, 5, 1.9, 2)) 
 
 	colidx<-(1:G)+2
 	for(i in 1:length(ff.idx)){
-		plot(1:ncol(test),test[id,],type="p",pch=16,cex=0.9,col=1,ylim=c(0,max(test,Ut)),xlim=c(0,ncol(test)),lty=linet[length(linet)],
-		xlab=" ", ylab=" ");par(new=T)
-		plot(1:ncol(test),Lt[i,],type="l",ylim=c(0,max(test,Ut)),col=colidx[i],lwd=1,lty=1,xlim=c(0,ncol(test)),xlab="mz values", ylab="log(intensity+1)");par(new=T)
+		plot(1:ncol(test),test[id,],type="p",pch=16,cex=0.9,col=1,ylim=c(0,max(test,Ut)+2),xlim=c(0,ncol(test)),lty=linet[length(linet)],
+		xlab=" ", ylab="");par(new=T)
+		plot(1:ncol(test),Lt[i,],type="l",ylim=c(0,max(test,Ut)+2),col=colidx[i],lwd=1,lty=1,xlim=c(0,ncol(test)),xlab="features", ylab="log(concentration+1)");par(new=T)
 		lines(1:ncol(test),Ut[i,],type="l",col=colidx[i],lwd=1,xlim=c(0,ncol(test)),lty=1)
 		#abline(v=pow.out,col=1,lty=2)
-		title(paste("Conformal prediction band Tn",sep="",i," for simulated data (alpha=",alpha,")"))
-		legend("topright",c("Outlier sample",paste("Conformal prediction band Tn",sep="",i)),
+		#title(paste("Conformal prediction band Tn",sep="",i," (alpha=",alpha,")"))
+		legend("topright",c(paste("Peaks within outlier sample ID=",sep="",id),paste("Marginal conformal prediction band Tn",sep="",i)),
 		col=c(1,colidx[i]),pch=c(16,NA),lty=c(NA,1),lwd=c(0,3))
 	}
 	
@@ -43,11 +44,11 @@ CPMS_band<-function(CPMS.obj=CPMS_result, test=test_dat, id=detected_sample_ID, 
 	}
 
 	## Placing x-values & x-axis label onto plot
-	text(pow.out[-c(1,nrow(pow.out)),],labels=pow.out[-c(1,nrow(pow.out)),1],pos=3,offset=1,font=0.5,cex=0.6,srt=90)
-	title("Outlier Sample ID",line=-3)
+	text(pow.out[-c(1,nrow(pow.out)),],labels=pow.out[-c(1,nrow(pow.out)),1],pos=3,offset=1,font=0.5,cex=0.8,srt=90)
+	title("Outlier feature ID",line=-5)
 	}
 
-	return(list(out.peakID=pow.out[-1,1]))
+	return(list(out.peakID=pow.out[-c(1,nrow(pow.out)),1]))
 }
 
 
